78001d6310247e0d5b48979ed9c15ddf6e032241
Porcelain <ai.red.porcelain@gmail.com>
2024-03-23 23:32:04 +0800

    finish movie search ignore

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -33,8 +33,12 @@ namespace eTicket.Controllers
 
 			if (!string.IsNullOrEmpty(searchString))
 			{
-				var filteredResult = allMovies.Where(n => n.Name.Contains(searchString) || n.Description.Contains(searchString)).ToList();
-				return View("Index", filteredResult);
+				//var filteredResult = allMovies.Where(n => n.Name.ToLower().Contains(searchString.ToLower()) || n.Description.ToLower().Contains(searchString.ToLower())).ToList();
+				//return View("Index", filteredResult);
+
+				var filterResultNew = allMovies.Where(n => string.Equals(n.Name, searchString, StringComparison.CurrentCultureIgnoreCase)
+				|| string.Equals(n.Description, searchString, StringComparison.CurrentCultureIgnoreCase)).ToList();
+				return View("Index", filterResultNew);
 			}
 
 			return View("Index", allMovies);
--- Program.cs
+++ Program.cs
@@ -53,7 +53,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+	pattern: "{controller=Movies}/{action=Index}/{id?}");
 //Seed database
 AppDbInitialize.Seed(app);
 AppDbInitialize.SeedUsersAndRolesAsync(app).Wait();
