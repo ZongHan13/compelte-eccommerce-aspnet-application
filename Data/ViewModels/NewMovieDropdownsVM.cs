a69b94d788d1900bc026dd9bae021ad6f96b4ce2
Porcelain <ai.red.porcelain@gmail.com>
2024-03-28 22:32:31 +0800

    format code

--- Data/ViewModels/NewMovieDropdownsVM.cs
+++ Data/ViewModels/NewMovieDropdownsVM.cs
@@ -2,16 +2,16 @@
 
 namespace eTicket.Data.ViewModels
 {
-	public class NewMovieDropdownsVM
-	{
-		public NewMovieDropdownsVM()
-		{
-			Producers = new List<Producer>();
-			Cinemas = new List<Cinema>();
-			Actors = new List<Actor>();
-		}
-		public List<Producer> Producers { get; set; }
-		public List<Cinema> Cinemas { get; set; }
-		public List<Actor> Actors { get; set; }
-	}
+    public class NewMovieDropdownsVM
+    {
+        public NewMovieDropdownsVM()
+        {
+            Producers = new List<Producer>();
+            Cinemas = new List<Cinema>();
+            Actors = new List<Actor>();
+        }
+        public List<Producer> Producers { get; set; }
+        public List<Cinema> Cinemas { get; set; }
+        public List<Actor> Actors { get; set; }
+    }
 }
